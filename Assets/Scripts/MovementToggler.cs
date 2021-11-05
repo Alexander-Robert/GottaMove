50284495834cf41ae62276f5f9dd2bafc08deb0c
williamlee <williamlee000@gmail.com>
2021-11-04 19:49:45 -0700

    created 3 buttons for movement (base, polished, distinct), you can continue to work on the ImprovedMovement script for distinct movement changes!

--- Assets/Scripts/MovementToggler.cs
+++ Assets/Scripts/MovementToggler.cs
@@ -8,13 +8,25 @@ public class MovementToggler : MonoBehaviour
     public void UseBase() {
         Behaviour script = player.GetComponent<ImprovedMovement>();
         if (script != null) script.enabled = false;
+        script = player.GetComponent<CelesteMovement>();
+        if (script != null) script.enabled = false;
         script = player.GetComponent<BaseMovement>();
         if (script != null) script.enabled = true;
     }
     public void UseImproved() {
         Behaviour script = player.GetComponent<BaseMovement>();
         if (script != null) script.enabled = false;
+        script = player.GetComponent<CelesteMovement>();
+        if (script != null) script.enabled = false;
         script = player.GetComponent<ImprovedMovement>();
+        if (script != null) script.enabled = true;
+    }
+    public void UseGroupImproved(){
+        Behaviour script = player.GetComponent<BaseMovement>();
+        if (script != null) script.enabled = false;
+        script = player.GetComponent<ImprovedMovement>();
+        if (script != null) script.enabled = false;
+        script = player.GetComponent<CelesteMovement>();
         if (script != null) script.enabled = true;
     }
 }
